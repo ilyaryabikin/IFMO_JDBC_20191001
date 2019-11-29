016ee00ebfbcb3381669a3f73bc36fc13ebd837f
ilyaryabikin <ilyaryabikin0708@gmail.com>
2019-11-29 23:22:42 +0300

    Implemented RowMapperFactory

--- src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
+++ src/main/java/com/efimchick/ifmo/web/jdbc/RowMapperFactory.java
@@ -1,10 +1,30 @@
 package com.efimchick.ifmo.web.jdbc;
 
 import com.efimchick.ifmo.web.jdbc.domain.Employee;
+import com.efimchick.ifmo.web.jdbc.domain.FullName;
+import com.efimchick.ifmo.web.jdbc.domain.Position;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class RowMapperFactory {
 
     public RowMapper<Employee> employeeRowMapper() {
-        throw new UnsupportedOperationException();
+        return new RowMapper<Employee>() {
+            @Override
+            public Employee mapRow(ResultSet resultSet) {
+                try {
+                    return new Employee(resultSet.getBigDecimal("id").toBigInteger(),
+                            new FullName(resultSet.getString("firstName"),
+                                    resultSet.getString("lastName"),
+                                    resultSet.getString("middleName")),
+                            Position.valueOf(resultSet.getString("position")),
+                            resultSet.getDate("hireDate").toLocalDate(),
+                            resultSet.getBigDecimal("salary"));
+                } catch (SQLException e) {
+                    throw new UnsupportedOperationException();
+                }
+            }
+        };
     }
 }
